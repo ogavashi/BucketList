3bfacdd014f4e4cf9a594f136f0d334fa2f9af08
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2024-01-26 16:35:11 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Oleg Gavashi on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
